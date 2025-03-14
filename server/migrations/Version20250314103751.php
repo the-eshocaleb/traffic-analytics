8d71351d7d1106aca2661b0728fab54ad7e12180
Caleb ESHO <110430708+the-eshocaleb@users.noreply.github.com>
2025-03-14 12:24:23 +0100

    feat: added api endpoint that returns json, seed database with data

---
+++ server/migrations/Version20250314103751.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250314103751 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE pagetraffic (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, page_id INTEGER NOT NULL, page_url VARCHAR(255) NOT NULL, traffic INTEGER NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE pagetraffic');
+    }
+}
